134545951c1ee5f1915e412ae2b8982fc450d7c0
Micheli <micheliknechtel@gmail.com>
2021-11-26 19:17:00 -0300

    add report.

--- src/main/java/com/example/delta/controller/OsController.java
+++ src/main/java/com/example/delta/controller/OsController.java
@@ -5,13 +5,28 @@ package com.example.delta.controller;
 import com.example.delta.domain.Os;
 import com.example.delta.dto.OsDto;
 import com.example.delta.service.OsService;
+import net.sf.jasperreports.engine.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -57,4 +72,36 @@ return modelAndView;}
         ModelAndView modelAndView = new ModelAndView("createOsNew");
         return modelAndView;
     }
+
+    @RequestMapping( value="/geraRelatorio",method = RequestMethod.GET)
+    public ResponseEntity<byte[]> getRelatorio(HttpServletResponse response, HttpServletRequest request) throws Exception {
+
+        try {
+            Connection conn;
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/loja", "root", "123");
+
+            HashMap parametros = new HashMap();
+
+            parametros.put("ID",  new Integer(request.getParameter("id")));
+            parametros.put("image",System.getProperty("user.dir")+"/image/Delta-Arrow-logo.png");
+
+            JasperReport jasperReport = JasperCompileManager.compileReport(System.getProperty("user.dir")+"/report/Blank_A4.jrxml");
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametros,conn);
+            System.out.println("Done! filling Jasper Report ");
+            JasperExportManager.exportReportToPdfFile(jasperPrint, System.getProperty("user.dir")+"/pdf/dynamic1.pdf");
+            System.out.println("Done!!!exporting the pdf report");
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+
+        InputStream n = new FileInputStream(new File(System.getProperty("user.dir")+"/pdf/dynamic1.pdf"));
+        InputStreamResource inputStreamResource = new InputStreamResource(n);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentLength(Files.size(Paths.get(System.getProperty("user.dir")+"/pdf/dynamic1.pdf")));
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        return new ResponseEntity(inputStreamResource, headers, HttpStatus.OK);
+
+    }
+
 }
