066b4c44673eba060656ead0dccc01452d9a5a26
Micheli <micheliknechtel@gmail.com>
2021-11-26 21:26:34 -0300

    Change annotation.

--- src/main/java/com/example/delta/domain/Client.java
+++ src/main/java/com/example/delta/domain/Client.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Client implements Serializable {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
--- src/main/java/com/example/delta/domain/Equipment.java
+++ src/main/java/com/example/delta/domain/Equipment.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 public class Equipment implements Serializable {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
     private String brand;
     private String model;
